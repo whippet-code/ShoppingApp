20a897ec5c5210348f15fdd6f53875c6646b5672
M. Ivkovic <m.ivkovic.dev@gmail.com>
2023-04-22 15:09:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+// Initialize Firebase
+const DBConfig = {
+  databaseURL:
+    "https://playground-e8497-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+const app = initializeApp(DBConfig);
+const db = getDatabase(app);
+const dbRef = ref(db, "shoppingList");
+
+// Add item to DB
+const addItem = (item) => {
+  push(dbRef, item);
+};
+
+// Remove item from DB
+const removeItem = (id) => {
+  remove(ref(db, `shoppingList/${id}`));
+};
+
+// Get items from DB
+const getItems = () => {
+  shoppingList.innerHTML = "";
+  onValue(dbRef, (snapshot) => {
+    let data = snapshot.val();
+    let allItems = Object.entries(data);
+    allItems.forEach((item) => {
+      shoppingList.innerHTML += appendItem(item);
+    });
+  });
+};
+
+// Append item to html
+const appendItem = (item) => {
+  return `<li id=${item[0]}>${item[1]}</li>`;
+};
+
+//clear input field
+const clearInput = () => {
+  userInput.value = "";
+};
+
+// set up html elements
+const addBtn = document.getElementById("add-to-list");
+const userInput = document.getElementById("item-to-add");
+const shoppingList = document.getElementById("shopping-list");
+
+// add event listener to add button
+addBtn.addEventListener("click", () => {
+  const userInputValue = userInput.value;
+  //clear input field
+  clearInput();
+  //add item to db
+  addItem(userInputValue);
+  //get items from db
+  getItems();
+});
+
+// event listener function to add to each list item when added
+shoppingList.addEventListener("click", (e) => {
+  let id = e.target.id;
+  removeItem(id);
+
+  //get items from db
+  getItems();
+});
+
+getItems();
